2923e10214b2c4acac9acd2422e6cc0c8bfaaddf
ndungu1mac <macgeorgendungu@gmail.com>
2022-08-24 11:41:14 +0300

    commit tree 548aff2a31fc07d142f1212df60adea48067808b

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+/*function scuberGreetingForFeet(){
+  let greetings;
+  if (scuberGreetingForFeet <= 400){
+     greetings = 'eligible for a free sample'
+  }
+  else if (scuberGreetingForFeet >2000){
+     greetings = 'Charge 30 dollars'
+  }
+  else if  (scuberGreetingForFeet >2500){
+     greetings = 'Ride not allowed'
+  }*/
+  //return greetings = `Eligible for a free sample`;
+  //else if scuberGreetingForFeet > 2000;
+     //return greetings = `Pay 30 dollars`;
+  //else return greetings = `Ride not allowed`
 }
 
+
+
 function ternaryCheckCity(){
-  // Write your code here!
+  const passengerMessage = ternaryCheckCity === 'NYC'? "Ok, sounds good." : "No go";
 }
 
 function switchOnCharmFromTip(){
