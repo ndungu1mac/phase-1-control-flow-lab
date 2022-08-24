bd115f690f99493bcb13d145b8448e5a9b60a195
ndungu1mac <macgeorgendungu@gmail.com>
2022-08-24 13:24:53 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  let greetings;
+  if (scuberGreetingForFeet <= 400){
+     greetings = 'eligible for a free sample'
+  }
+  else if (scuberGreetingForFeet >2000){
+     greetings = 'Charge 30 dollars'
+  }
+  else if  (scuberGreetingForFeet >2500){
+     greetings = 'Ride not allowed'
+  }
+  //return greetings = `Eligible for a free sample`;
+  //else if scuberGreetingForFeet > 2000;
+     //return greetings = `Pay 30 dollars`;
+  //else return greetings = `Ride not allowed`
 }
 
+
+
 function ternaryCheckCity(){
-  // Write your code here!
+  const passengerMessage = ternaryCheckCity = 'NYC'? "Ok, sounds good." : "No go";
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  const switchOnCharmFromTip = ''
+  let message;
+  switch(OnCharmFromTip){
+    case 'generous':
+     message = "Thank you so much";
+     break;
+    case  'not as generous':
+     message = "Thank you ";
+     break;
+     default:
+        console.log("Bye.");
+      break;
+
+  }
+ 
+
 }
